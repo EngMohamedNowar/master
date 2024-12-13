ae9bbc820791e677a45a8f0fed0945e3c2e9a5a9
EngMohamedNowar <mohamednowar@icloud.com>
2024-12-13 04:34:58 +0200

    fifth commit

--- product/filters.py
+++ product/filters.py
@@ -5,8 +5,10 @@ from .models import Product
 class ProductsFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='iexact')
     keyword = django_filters.filters.CharFilter(field_name ="name", lookup_expr="icontains")
+    minprice = django_filters.filters.NumberFilter(field_name ="price" or 0 ,lookup_expr ="gte")
+    maxprice = django_filters.filters.NumberFilter(field_name ="price" or 10000 ,lookup_expr ="lte")
 
     class Meta:
         model = Product
         # fields = ['category', 'brand']
-        fields = ['category', 'brand','keyword']
+        fields = ['category', 'brand','keyword','minprice', 'maxprice']
--- product/views.py
+++ product/views.py
@@ -5,16 +5,21 @@ from rest_framework.decorators import api_view
 
 from.models import Product
 from.serializers import ProductSerializer
-
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 
 @api_view(['GET'])
 def get_all_products(request):
     # products =Product.objects.all()
     filter = ProductsFilter(request.GET,queryset=Product.objects.all().order_by('id'))
+    count = filter.qs.count()
+    restPage = 2
+    paginator =PageNumberPagination()
+    paginator.page_size = restPage
+    queryset = paginator.paginate_queryset(filter.qs,request)
     # serializer = ProductSerializer(products, many=True)
-    serializer = ProductSerializer(filter.qs, many=True)
-    return Response({'products':serializer.data})
+    serializer = ProductSerializer(queryset, many=True)
+    return Response({'products':serializer.data,'per pages':restPage,'products count':count})
 @api_view(['GET'])
 def get_by_id_products(request, pk):
     product = get_object_or_404(Product, id =pk)
